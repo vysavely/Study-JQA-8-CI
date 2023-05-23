fa4af59b576e4ae371cd399758fc30ad555143a5
vyacheslav.a.savelye <s02a@ya.ru>
2023-05-23 19:23:14 +0300

    Add JaCoCo plugin

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -10,9 +10,22 @@ public class MaxServiceTest {
         int a = 5;
         int b = 3;
 
-        int expected = a;
+        int expected = a; // по мнению IDEA данная переменная избыточна, но здесь она нужна для наглядности
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void additionalTestForB() {
+        MaxService service = new MaxService();
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
